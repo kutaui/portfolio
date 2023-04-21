23667abdeea857ab48f3cb744df3f06d535e94df
Kutay <kutaybekleric@gmail.com>
2023-04-21 23:09:38 +0300

    finish mobile version

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,22 +14,15 @@ export const About = ({ onRefUpdate }) => {
     <>
       <section ref={aboutRef} className={styles.about}>
         <h1>About</h1>
-        <p>
+        <p lang="en">
           Kutay Bekleriç is a highly motivated web developer with a strong focus
           on front-end development. Over the past year, Kutay has dedicated
-          himself to mastering the MERN stack, building a solid foundation in
-          HTML, CSS (SASS), JavaScript, React, Tailwind, MongoDB, Node.js, Git,
-          Go and design tools like Figma and Adobe XD. Despite having no prior
-          work experience, Kutay's commitment to learning and improving his
-          skills is unwavering. He is a quick learner with excellent
-          communication skills developed through his experience as a Vice
-          President in a college community. With a Bachelor's in American
-          Culture and Literature from Dokuz Eylul University at İzmir, Kutay
-          brings a unique perspective and an eye for detail to his work. His
-          volunteer work at Gençİzmir has provided him with valuable experience
-          in marketing, branding, and social media management. Kutay is excited
-          to contribute his technical knowledge and communication skills to a
-          dynamic work environment and make a positive impact as a web
+          himself to mastering the MERN stack. He is a quick learner with
+          excellent communication skills developed through his experience as a
+          Vice President in a college community. With a Bachelor's in American
+          Culture and Literature from Dokuz Eylul University at İzmir, Kutay is
+          excited to contribute his technical knowledge and communication skills
+          to a dynamic work environment and make a positive impact as a web
           developer.
         </p>
       </section>
