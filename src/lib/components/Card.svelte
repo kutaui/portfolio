912d2549219be56ab51593a94bb4962252556d79
kutaui <kutaybekleric@gmail.com>
2024-04-09 15:33:25 +0300

    finish

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -8,13 +8,34 @@
 		{description}
 	</p>
 </div>
-<div class="flex items-center justify-end">
-	<img
-		alt="Acme Corporation Website Redesign"
-		class="rounded-lg object-cover"
-		height="200"
-		src={image}
-		style="aspect-ratio: 400/300; "
-		width="400"
-	/>
+
+<div class="flex items-center justify-end relative">
+	<div class="relative">
+		<img
+			alt="Acme Corporation Website Redesign"
+			class="rounded-lg object-cover"
+			height="200"
+			src={image}
+			style="aspect-ratio: 400/300; "
+			width="400"
+		/>
+		<div
+			class="absolute inset-0 flex items-center justify-center transition-opacity duration-200 opacity-0 bg-black bg-opacity-50 hover:opacity-100 rounded-lg"
+		>
+			<div class="flex justify-between w-48">
+				<div class="flex flex-col items-center">
+					<a href={source} target="_blank">
+						<img src="/github-mark-white.png" alt="GitHub Icon" class="w-12" />
+					</a>
+					<a href={source} target="_blank" class="text-white font-bold">Source</a>
+				</div>
+				<div class="flex flex-col items-center">
+					<a href={live} target="_blank">
+						<img src="/internet.png" alt="World Wide Web Icon" class="w-12" />
+					</a>
+					<a href={live} target="_blank" class="text-white font-bold">Live</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,19 +6,20 @@
 	<section class="grid gap-12 lg:gap-16 px-4 md:px-6">
 		<div class="mx-auto max-w-5xl grid gap-6 lg:grid-cols-2">
 			<Card
+				title="BeeMusic"
+				description="Beemusic is a fullstack social media app that allows users to share their music taste with others. Users can create posts, like posts, comment on posts and follow each other. The backend is built with Express.js and Apollo GraphQL. The frontend is built with Next.js and Apollo GraphQL. The database is PostgreSQL and Prisma is used as an ORM."
+				image="./beemusic.png"
+				source="https://github.com/kutaui/BeeMusic"
+				live="https://beemusic.kutaybekleric.com/"
+			/>
+			<Card
 				title="Pizzeria"
 				description="Pizzeria is a SvelteKit app that allows users to order their favorite dishes. The app also includes an in-depth admin panel for the restaurant owners to manage their orders, dishes and users. The app is made with 0 API routes and only uses form actions and the new Svelte runes."
 				image="./pizzeria.png"
 				source="https://github.com/kutaui/svelte-pizzeria"
 				live="https://pizzeria.kutaybekleric.com/"
 			/>
-			<Card
-				title="BeeMusic"
-				description="Beemusic is a fullstack social media app that allows users to share their music taste with others. Users can create posts, like posts, comment on posts(more stuff to come). The backend is built with Express.js and Apollo GraphQL. The frontend is built with Next.js and Apollo GraphQL. The database is PostgreSQL and Prisma is used as an ORM."
-				image="./beemusic.png"
-				source="https://github.com/kutaui/BeeMusic"
-				live="https://beemusic.kutaybekleric.com/"
-			/>
+
 			<Card
 				title="ThoughtPocket"
 				description="MERN stack app with a rich text editor that enables users to store their thoughts and access them from anywhere. The app also includes a dark mode and a light mode, autosave feature and JWT authentication."
@@ -41,5 +42,9 @@
 				live="https://blog.kutaybekleric.com/"
 			/>
 		</div>
+		<hr class="border-black" />
+		<a href="https://kutay.boo" target="_blank" class="justify-self-center -mb-5 -mt-6">
+			<img src="/internet-black.png" alt="GitHub Icon" class="w-12" />
+		</a>
 	</section>
 </main>
